5c87a9dc0b3be3127f9c818dad9bcd9e023a8728
amartynov <h8machine@h8machine-2.local>
2016-05-25 00:23:43 +0300

    Add home page controller, add footer and header partials, add styles

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,10 @@
 class Job < ActiveRecord::Base
   include PgSearch
-  pg_search_scope :search_by_title, :against => :title
+  pg_search_scope :search_by_title, against: :title
+  pg_search_scope :search_by_company, against: :company
+  pg_search_scope :search_by_exact_match,
+                   against: [ :title, :company, :description, :location],
+                   using: {
+                       tsearch: { normalization: 2 }
+                   }
 end
